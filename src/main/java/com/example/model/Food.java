9228052cf37c3f9acb405e9c7e604d220a422f76
long113112113 <longnn@sparkminds.net>
2025-04-04 16:51:47 +0700

    Add Food class with constructor and getter methods

--- src/main/java/com/example/model/Food.java
+++ src/main/java/com/example/model/Food.java
@@ -8,4 +8,30 @@ public class Food {
     private int price;
     private int stock;
     private Date expired_date;
+
+    public Food(int id, String name, int price, int stock, Date expired_date) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+        this.expired_date = expired_date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+    public Date getExpired_date() {
+        return expired_date;
+    }
 }
